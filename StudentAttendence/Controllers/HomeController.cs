c16a9831cd0049c93821335c555aa52a9b5d82fb
Mohammad Owais <o-wais72@live.com>
2017-08-15 00:06:08 +0500

    StudentAttendence
    abc

---
+++ StudentAttendence/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using StudentAttendence.Models;
+using StudentAttendenceSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentAttendence.Controllers
+{
+    public class HomeController : Controller
+    {
+        public StudentManagmentSystem db = new StudentManagmentSystem();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var bat = new Class1();
+            bat.lab = db.Batches.Select(x=>new SelectListItem {Value=x.Id.ToString(),Text=x.BatchCode }).ToList();
+
+           //var batch = db.Batches.ToList();
+            
+           // ViewBag.batch = new SelectList(list,"id","BatchCode");
+
+
+
+
+            return View(bat);
+        }
+        
+
+        [HttpPost]
+        [HttpGet]
+        public ActionResult Index(string batchcode, string StudentName, string atten, string date, StudentAtten att)
+        {
+
+            att.BatchCode = batchcode;
+            att.StudentName = StudentName;
+            att.Attendence = atten;
+            att.Date = date;
+            db.StudentAttens.Add(att);
+            db.SaveChanges();
+            return View();
+        }
+
+
+        public JsonResult GetStdList(int Id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var CityList = db.Students.Where(x => x.BatchId== Id).ToList();
+            return Json(CityList, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        public ActionResult ShowAttendence()
+        {
+
+
+            var std = db.StudentAttens.ToList();
+            ViewBag.StdAtt = std;
+            return View();
+        }
+       
+
+
+    }
+}
---
+++ StudentAttendence/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentAttendence.Models
+{
+    public class Class1
+    {
+        public IEnumerable<SelectListItem> lab { get; set; }
+        public int Id{ get; set; }
+        public int StudentId { get; set; }
+
+    }
+}
